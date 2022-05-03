31be82e0b5060e83b6af419b3f5a601f66532349
Chinthaka1 <sajithdoo@gmail.com>
2022-05-03 23:44:38 +0800

    Interface added

---
+++ DiUSBowlingScore/src/main/java/org/dius/bowling/sb/BowlingGame.java
@@ -0,0 +1,7 @@
+package org.dius.bowling.sb;
+
+public interface BowlingGame {
+    public void roll(int noOfPins);
+
+    public int score();
+}
---
+++ DiUSBowlingScore/src/main/java/org/dius/bowling/sb/BowlingGameImpl.java
@@ -0,0 +1,21 @@
+package org.dius.bowling.sb;
+
+public class BowlingGameImpl implements BowlingGame {
+
+
+    public BowlingGameImpl() {
+    }
+
+    @Override
+    public void roll(int noOfPins) {
+
+    }
+
+
+    @Override
+    public int score() {
+        return 0;
+    }
+
+
+}
